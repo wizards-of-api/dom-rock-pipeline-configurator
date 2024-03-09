08b4b86ceae254e4ab0dc5cb2e30c49db59f734b
pauloarantesmachado <pauloantoniomachado@hotmail.com>
2024-03-09 11:36:52 -0300

    :tada: create web-server

---
+++ web-server/src/test/java/com/domrock/configurator/ConfiguratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domrock.configurator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfiguratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
