43e1de22bc4189ee39e9a03b37ef19ab964a91e5
Matheus <mm.leitea@outlook.com>
2024-04-04 10:56:27 -0300

    🔧 Configure eslint and prettier

--- web-client/.eslintrc.cjs
+++ web-client/.eslintrc.cjs
@@ -3,13 +3,20 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
+  'plugins': [
+    '@stylistic/js',
+  ],
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    '@stylistic/js/comma-dangle': ['warn', 'always-multiline'],
+    '@stylistic/js/eol-last': ['error', 'always'],
+  },
 }
