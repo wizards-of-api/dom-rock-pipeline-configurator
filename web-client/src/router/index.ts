7bab2206581624fdb0a003725d81028e1293b3ff
Matheus <mm.leitea@outlook.com>
2024-04-04 10:20:01 -0300

    🎉 Clear VUE Demo project and prepare ours
    Add sass/scss pre-processor, configure our root colors and font, configure base page element displayment, remove Vue Demo views and components, configure route

--- web-client/src/router/index.ts
+++ web-client/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import ListView from '../views/ListView.vue'
+import SandboxView from '@/views/SandboxView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,15 +8,20 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: ListView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/lz-config',
+      name: 'lz-config',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/LZConfigView.vue')
+    },
+    {
+      path: '/sandbox',
+      name: 'sandbox',
+      component: SandboxView
     }
   ]
 })
