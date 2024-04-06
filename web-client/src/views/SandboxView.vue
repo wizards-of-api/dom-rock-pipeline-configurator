5469cb358cee33d9d4321c6d34b5c7d7e8e47bba
Matheus Marciano <mm.leitea@outlook.com>
2024-04-06 18:10:35 -0300

    :construction: (WIP) add vue components

--- web-client/src/views/SandboxView.vue
+++ web-client/src/views/SandboxView.vue
@@ -1,10 +1,26 @@
 <script setup lang="ts">
+import DomRockButton from '@/components/DomRockButton.vue'
+import { ref } from 'vue'
 /*
 This view is suposed to test components in Dev mode
 No route should lead to this view
 */
 
 // TODO Remove this view on production
+
+const list = [0, 1, 2, 3]
+
+const foo = {
+	bar: 1,
+	baz: 2,
+	foz: 3,
+}
+
+let showHello = ref(false)
+const toggleHello = () => {
+	console.log(foo)
+	showHello.value = !showHello.value
+}
 </script>
 
 <template>
@@ -14,6 +30,12 @@ No route should lead to this view
     <p>
       <span class="section-title">Landing Zone</span>
     </p>
+
+    <DomRockButton button-type="safe" :click-behavior="toggleHello">ABC</DomRockButton>
+    <h1 v-if="showHello">Hello World!</h1>
+    <h1 v-for="number in list" :key="number">
+      {{ number }}
+    </h1>
   </div>
 </template>
 
