0b3ed9409b79b78d4e73ac421530b181edf18341
Otavio-CB <otaviocalderanadm@gmail.com>
2024-04-05 22:23:21 -0300

    ✨ Add excel to json converter

---
+++ web-server/src/main/java/com/domrock/configurator/utils/ArrayListToJson.java
@@ -0,0 +1,14 @@
+package com.domrock.configurator.utils;
+
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+
+public class ArrayListToJson {
+  public String jsonConversion() {
+    Gson gson = new Gson();
+    CsvConverter csv = new CsvConverter();
+    ArrayList<String> sheet = csv.csvConverter();
+    return (gson.toJson(sheet));
+  }
+}
