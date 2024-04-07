44500fe9166c2579e247ceb2fd30f5bb541b5447
ChavesVini <vinidasilva2004@outlook.com>
2024-04-06 22:32:55 -0300

    :sparkles: add oldExcelConverter method and rename excelConverter to newExcelConverter

--- web-server/src/main/java/com/domrock/configurator/utils/CsvConverter.java
+++ web-server/src/main/java/com/domrock/configurator/utils/CsvConverter.java
@@ -3,6 +3,8 @@ package com.domrock.configurator.utils;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.usermodel.HSSFSheet; 
 import org.apache.poi.ss.usermodel.Cell;
 
 import org.apache.commons.csv.CSVFormat;
@@ -16,12 +18,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class CsvConverter {
-    public ArrayList<String> excelConverter() {
+    public ArrayList<String> newExcelConverter(String filePath) {
         int index = 0;
         ArrayList<String> headers = new ArrayList<String>();
 
         try {
-            FileInputStream file = new FileInputStream("web-server/people.xlsx");
+            FileInputStream file = new FileInputStream(filePath);
             XSSFWorkbook wkbk = new XSSFWorkbook(file);
             XSSFSheet sheet = wkbk.getSheetAt(0);
             Row row = sheet.getRow(0);
@@ -39,11 +41,36 @@ public class CsvConverter {
         }
         return headers;
     }
-    public ArrayList<String> csvConverter() {
+
+    public ArrayList<String> oldExcelConverter(String filePath) {
+        int index = 0;
+        ArrayList<String> headers = new ArrayList<String>();
+    
+        try {
+            FileInputStream file = new FileInputStream(filePath);
+            HSSFWorkbook hkbk = new HSSFWorkbook(file);
+            HSSFSheet sheet = hkbk.getSheetAt(0);
+            Row row = sheet.getRow(0);
+    
+            for (int i = 0; i < (row.getLastCellNum()); i++) {
+                Cell cell = row.getCell(i);
+                headers.add(index + "");
+                headers.add(cell.toString());
+                index += 1;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return headers;
+    }
+
+    public ArrayList<String> csvConverter(String filePath) {
         int index = 0;
         ArrayList<String> headers = new ArrayList<String>();
         try {
-            FileReader reader = new FileReader("web-server/people.csv");
+            FileReader reader = new FileReader(filePath);
             CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
             for (CSVRecord record : csvParser) {
                 for (int i=0; i<(csvParser.getRecords().size()); i++) {
