10e9e2f4dde3282c437c6544ae436e13424846ba
Matheus <mm.leitea@outlook.com>
2024-04-09 10:36:54 -0300

    🐛 Fix dropdown being input[type="text"]

--- web-client/src/components/DRDropDown.vue
+++ web-client/src/components/DRDropDown.vue
@@ -1,18 +1,27 @@
 <script setup lang="ts">
 type Props = {
 	title: string
+	optionList: string[]
 }
-const { title } = defineProps<Props>()
+const { title, optionList } = defineProps<Props>()
 </script>
 <template>
 	<div class="wrapper">
 		<span>{{ title }}</span>
-		<input class="input" type="text" />
+		<select class="input">
+			<option v-for="option in optionList" :key="option">{{ option }}</option>
+		</select>
 	</div>
 </template>
 <style scoped lang="scss">
 .wrapper {
 	display: flex;
 	flex-direction: column;
+}
+option {
+	background: var(--color-background-soft);
+	&:focus, &:hover {
+		background: var(--color-button-careful);
+	}
 }
 </style>
