19b87c558ed2ebbd77aa3137fd1c185251a86cce
Otavio-CB <otaviocalderanadm@gmail.com>
2024-04-11 22:48:25 -0300

    🚧 Work in Progress

--- web-server/src/main/java/com/domrock/configurator/Controller/ListViewController.java
+++ web-server/src/main/java/com/domrock/configurator/Controller/ListViewController.java
@@ -8,7 +8,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +27,10 @@ public class ListViewController {
         return ResponseEntity.ok(page);
     }
 
+    @GetMapping("/{id}")
+    @Transactional
+    public ResponseEntity details(@PathVariable Integer id){
+        LZMetadataConfig config = lzRepository.getReferenceById(id);
+        return ResponseEntity.ok(config);
+    }
 }
