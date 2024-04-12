196ac782feb9223a664f3cc98f5b9bb58a5f74eb
Matheus Marciano <mm.leitea@outlook.com>
2024-04-11 21:55:14 -0300

    :rewind: Fix wrong conflict resolution

--- web-server/src/main/java/com/domrock/configurator/Controller/ConfigController.java
+++ web-server/src/main/java/com/domrock/configurator/Controller/ConfigController.java
@@ -1,6 +1,8 @@
 package com.domrock.configurator.Controller;
 
 import com.domrock.configurator.Model.ConfigModel.DTOConfig.*;
+import com.domrock.configurator.Services.ArrayListToJson;
+import com.domrock.configurator.Services.CsvConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,11 +11,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.domrock.configurator.Model.ConfigModel.ColumnConfig;
 import com.domrock.configurator.Model.ConfigModel.LZMetadataConfig;
 import com.domrock.configurator.Services.ColumnConfigServices;
 import com.domrock.configurator.Services.LZMetadataConfigServices;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/lz-config")
@@ -23,6 +29,35 @@ public class ConfigController {
 
     @Autowired
     ColumnConfigServices lzColumnConfigServices;
+
+    @Autowired
+    private ArrayListToJson arrayListToJson;
+
+    @Autowired
+    private CsvConverter csvConverter;
+
+    @GetMapping("/new-excel-to-json")
+    public String newExcelToJson(@RequestParam String filePath) {
+        return arrayListToJson.newExcelToJson(filePath);
+    }
+
+    @GetMapping("/old-excel-to-json")
+    public String oldExcelToJson(@RequestParam String filePath) {
+        return arrayListToJson.oldExcelToJson(filePath);
+    }
+
+    @GetMapping("/upload-csv")
+    public ResponseEntity<ListColumnResponseDTO> csvToJson(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("hasHeader") boolean hasHeader,
+            @RequestParam("fileExtension") String fileExtension,
+            @RequestParam("separator") String separator,
+            @RequestParam("separator") String fileName
+            ) {
+        List<ColumnResponseDTO> columns = csvConverter.createFileCsv(file, fileName, fileName);
+        ListColumnResponseDTO responseJson = new ListColumnResponseDTO(columns);
+        return ResponseEntity.ok(responseJson);
+    }
 
     @CrossOrigin(origins = "http://localhost:5173")
     @PostMapping("/save")
