47bfad3c3bdd967ae406fc5acdf781c86612d95d
pauloarantesmachado <pauloantoniomachado@hotmail.com>
2024-04-11 22:13:14 -0300

    :construction: create get paginable for listView screen

--- web-server/src/main/java/com/domrock/configurator/Controller/ListViewController.java
+++ web-server/src/main/java/com/domrock/configurator/Controller/ListViewController.java
@@ -1,7 +1,28 @@
-package com.domrock.configurator.controller;
+package com.domrock.configurator.Controller;
 
-import org.springframework.data.jpa.repository.Query;
 
+import com.domrock.configurator.Interface.LZMetadataConfigInterface;
+import com.domrock.configurator.Model.ConfigModel.LZMetadataConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/list-view")
 public class ListViewController {
+
+    @Autowired
+    LZMetadataConfigInterface lzRepository;
+    @GetMapping
+    public ResponseEntity<Page<LZMetadataConfig>> listDoctor(@PageableDefault(size = 16, sort={"name"}) Pageable paginator){
+        Page<LZMetadataConfig> page = lzRepository.findAll(paginator);
+        return ResponseEntity.ok(page);
+    }
 
 }
