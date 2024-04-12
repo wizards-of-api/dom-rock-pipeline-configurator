6c24f67cb972e44ca94eb5ca18f2f9ece9a79367
Otavio-CB <otaviocalderanadm@gmail.com>
2024-04-12 19:58:54 -0300

    ✨ Add configuration query by id

--- web-server/src/main/java/com/domrock/configurator/Controller/ConfigController.java
+++ web-server/src/main/java/com/domrock/configurator/Controller/ConfigController.java
@@ -1,12 +1,14 @@
 package com.domrock.configurator.Controller;
 
+import com.domrock.configurator.Interface.LZMetadataConfigInterface;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 import com.domrock.configurator.Model.ConfigModel.ColumnConfig;
 import com.domrock.configurator.Model.ConfigModel.LZMetadataConfig;
@@ -15,6 +17,7 @@ import com.domrock.configurator.Model.ConfigModel.DTOConfig.DataConfigDTO;
 import com.domrock.configurator.Model.ConfigModel.DTOConfig.MetadataConfigDTO;
 import com.domrock.configurator.Services.ColumnConfigServices;
 import com.domrock.configurator.Services.LZMetadataConfigServices;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/config")
@@ -24,6 +27,9 @@ public class ConfigController {
 
     @Autowired
     ColumnConfigServices lzColumnConfigServices;
+
+    @Autowired
+    LZMetadataConfigInterface lzRepository;
 
     @PostMapping
     public ResponseEntity<LZMetadataConfig> postConfig(@RequestBody DataConfigDTO data){
@@ -40,5 +46,23 @@ public class ConfigController {
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+
+    @RequestMapping("/list-view")
+    @GetMapping
+    public ResponseEntity<Page<LZMetadataConfig>> listDoctor(@PageableDefault(size = 16, sort={"name"}) Pageable paginator){
+        Page<LZMetadataConfig> page = lzRepository.findAll(paginator);
+        return ResponseEntity.ok(page);
+    }
+
+    @GetMapping("/{id}")
+    @Transactional
+    public LZMetadataConfig details(@PathVariable Integer id){
+        var configNum = lzRepository.findById(id);
+        if(configNum.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return configNum.get();
     }
 }
--- web-server/src/main/java/com/domrock/configurator/Interface/LZMetadataConfigInterface.java
+++ web-server/src/main/java/com/domrock/configurator/Interface/LZMetadataConfigInterface.java
@@ -1,9 +1,8 @@
 package com.domrock.configurator.Interface;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.domrock.configurator.Model.ConfigModel.LZMetadataConfig;
 
 public interface LZMetadataConfigInterface extends JpaRepository<LZMetadataConfig, Integer>{
-    
+
 }
