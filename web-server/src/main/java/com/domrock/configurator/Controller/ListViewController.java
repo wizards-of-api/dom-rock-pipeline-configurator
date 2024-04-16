ae5a4277c224013129d2da4b4c152c823de8f502
ChavesVini <vinidasilva2004@outlook.com>
2024-04-16 00:19:10 -0300

    :art: removed unused imports

--- web-server/src/main/java/com/domrock/configurator/Controller/ListViewController.java
+++ web-server/src/main/java/com/domrock/configurator/Controller/ListViewController.java
@@ -1,20 +1,13 @@
 package com.domrock.configurator.Controller;
 
-
 import com.domrock.configurator.Interface.LZMetadataConfigInterface;
 import com.domrock.configurator.Model.ConfigModel.LZMetadataConfig;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import lombok.AllArgsConstructor;
-
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -54,6 +47,5 @@ public class ListViewController {
         }
         lzRepository.delete(delete.get());
         return ResponseEntity.noContent().build();
-        
     }
 }       
