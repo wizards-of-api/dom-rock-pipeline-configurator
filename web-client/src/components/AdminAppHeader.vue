f5c5f4dc248419140d8a2f20ad4b5d916581e565
julioFreitas <freitas27j@gmail.com>
2024-05-16 21:55:23 -0300

    :wrench: In this task, I focused on enhancing the admin experience by developing a new home screen and associated components for the web product. I designed and implemented a new home screen specifically for administrators, which includes navigation buttons linking to various functionalities: 'New User' for user creation, 'New Company' for company creation, 'Action Log' for viewing the activity log, and 'Dashboards' for accessing dashboards. Additionally, I modified the button sizes to ensure they fit well within the layout, preventing any breaking or misalignment issues. Furthermore, I created a new header component tailored specifically for admin pages, ensuring a consistent and professional look across all admin-related screens. These updates provide a streamlined and user-friendly interface for administrators, improving navigation and overall user experience within the admin section of the web application.

---
+++ web-client/src/components/AdminAppHeader.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <router-link to="/admin-home">
+      <img src="../assets/dom-rock-logo.png">
+    </router-link>
+    <div class="user">
+      <h3>{{ $route.name }}</h3>
+      <img src="../assets/user-icon.png" height="60px"/>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+img {
+  max-width: 154px;
+}
+header {
+  display:flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 2rem;
+  background: var(--color-background-soft);
+  border-bottom: solid 1px var(--color-separator);
+  min-width: 100%;
+}
+.user{
+  display:flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 20px;
+}
+</style>
