f426e2981a0b576a1e5be1d5ed322ceeddc58cd1
AndreWakugawa <andre.wakugawa@outlook.com>
2024-05-19 21:06:29 -0300

    ✨feat: findByEmail, add and remove user permission

--- web-server/src/main/java/com/domrock/configurator/Services/UserService.java
+++ web-server/src/main/java/com/domrock/configurator/Services/UserService.java
@@ -1,7 +1,9 @@
 package com.domrock.configurator.Services;
 
+import com.domrock.configurator.Interface.PermissionRepository;
 import com.domrock.configurator.Interface.UserRepository;
 import com.domrock.configurator.Model.ConfigModel.DTOConfig.UserDTO;
+import com.domrock.configurator.Model.ConfigModel.Permission;
 import com.domrock.configurator.Model.ConfigModel.User;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,9 @@ import java.util.stream.Collectors;
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private PermissionRepository permissionRepository;
 
     @Autowired
     private ModelMapper modelMapper;
@@ -34,5 +39,26 @@ public class UserService {
                     .map((user) -> modelMapper.map(user, UserDTO.class))
                     .collect(Collectors.toList());
         }
+    }
+
+    public UserDTO findByEmail(String email) {
+        User user = userRepository.findById(email).orElseThrow(NoSuchElementException::new);
+        return modelMapper.map(user, UserDTO.class);
+    }
+
+    public UserDTO addUserPermission(String email, String permissionType) {
+        User user = userRepository.findById(email).orElseThrow(NoSuchElementException::new);
+        Permission permission = permissionRepository.findPermissionByType(permissionType);
+        user.getPermissions().add(permission);
+        userRepository.save(user);
+        return modelMapper.map(user, UserDTO.class);
+    }
+
+    public UserDTO removeUserPermission(String email, String permissionType) {
+        User user = userRepository.findById(email).orElseThrow(NoSuchElementException::new);
+        Permission permission = permissionRepository.findPermissionByType(permissionType);
+        user.getPermissions().remove(permission);
+        userRepository.save(user);
+        return modelMapper.map(user, UserDTO.class);
     }
 }
