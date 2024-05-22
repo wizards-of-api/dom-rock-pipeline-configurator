e1854034e49c99c9680e259004f665cd9ce50664
AndreWakugawa <andre.wakugawa@outlook.com>
2024-05-21 23:03:47 -0300

    ✨feat: findBronzeValidated query in lzconfig repo

--- web-server/src/main/java/com/domrock/configurator/Interface/LZMetadataConfigInterface.java
+++ web-server/src/main/java/com/domrock/configurator/Interface/LZMetadataConfigInterface.java
@@ -1,9 +1,19 @@
 package com.domrock.configurator.Interface;
 
+import com.domrock.configurator.Model.ConfigModel.DTOConfig.BronzeValidatedDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.domrock.configurator.Model.ConfigModel.LZMetadataConfig;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface LZMetadataConfigInterface extends JpaRepository<LZMetadataConfig, Integer>{
+
+    @Query(value = "SELECT DISTINCT l.file_id, l.file_config_name " +
+            "FROM lz_config l JOIN file_column c " +
+            "ON l.file_id = c.file_id " +
+            "WHERE c.column_valid != 0 AND c.column_active != 0", nativeQuery = true)
+    public List<Object[]> findBronzeValidated();
     
 }
