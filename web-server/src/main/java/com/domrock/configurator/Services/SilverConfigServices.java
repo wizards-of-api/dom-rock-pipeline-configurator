5cbac20185de5fb4017277fd0b2fb6b8a213b257
Vinícius Chaves <126925449+ChavesVini@users.noreply.github.com>
2024-05-23 22:28:37 -0300

    :sparkles: modify services

--- web-server/src/main/java/com/domrock/configurator/Services/SilverConfigServices.java
+++ web-server/src/main/java/com/domrock/configurator/Services/SilverConfigServices.java
@@ -2,23 +2,33 @@ package com.domrock.configurator.Services;
 
 import java.util.Optional;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.domrock.configurator.Interface.SilverConfigInterface;
 import com.domrock.configurator.Model.ConfigModel.SilverConfig;
+import com.domrock.configurator.Model.ConfigModel.DTOConfig.SilverConfigDTO;
 
 @Service
 public class SilverConfigServices {
 
     @Autowired
-    SilverConfigInterface silverConfigInterface;
+    private SilverConfigInterface silverConfigInterface;
 
-    public SilverConfig saveSilverConfig(SilverConfig lzConfig) {
-        return silverConfigInterface.save(lzConfig);
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public SilverConfigDTO saveSilverConfig(SilverConfigDTO silverConfigDTO) {
+        SilverConfig silverConfig = modelMapper.map(silverConfigDTO, SilverConfig.class);
+        silverConfigInterface.save(silverConfig);
+        return silverConfigDTO;
     }
 
-    public Optional<SilverConfig> getConfigById(Integer id) {
-        return silverConfigInterface.findById(id);
+    public SilverConfigDTO getConfigById(Integer id) {
+        Optional<SilverConfig> silverConfig = silverConfigInterface.findById(id);
+        SilverConfigDTO silverConfigDTO = modelMapper.map(silverConfig.get(), SilverConfigDTO.class);
+        return silverConfigDTO;
     }
+
 }
