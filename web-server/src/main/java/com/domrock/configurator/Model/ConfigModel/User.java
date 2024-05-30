7a0eb5efe22b7635baa0eb2139eddc6600089baf
AndreWakugawa <andre.wakugawa@outlook.com>
2024-05-30 00:03:15 -0300

    ✨feat: UserDetails implementation in User entity
    Update in User Entity to implement UserDetails and be fit for use by Spring Security.

--- web-server/src/main/java/com/domrock/configurator/Model/ConfigModel/User.java
+++ web-server/src/main/java/com/domrock/configurator/Model/ConfigModel/User.java
@@ -5,7 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
 @Getter
@@ -14,7 +20,7 @@ import java.util.Set;
 @NoArgsConstructor
 @Entity
 @Table(name = "user")
-public class User {
+public class User implements UserDetails {
 
     @Column(name = "name", nullable = false)
     private String name;
@@ -44,4 +50,42 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "company_cnpj")
     )
     private Set<Company> companies;
+
+    LocalDateTime createdAt;
+
+    LocalDateTime updatedAt;
+
+    @Enumerated(EnumType.STRING)
+    Role role;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
