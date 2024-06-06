7e98c93c0bb9f04a7d302831d68e94ace18f5f4d
AndreWakugawa <andre.wakugawa@outlook.com>
2024-06-05 23:13:35 -0300

    🩹fix: getAuthorities method on new role struct

--- web-server/src/main/java/com/domrock/configurator/Model/ConfigModel/User.java
+++ web-server/src/main/java/com/domrock/configurator/Model/ConfigModel/User.java
@@ -51,7 +51,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()));
+        return Collections.singletonList(new SimpleGrantedAuthority(permission.getType()));
     }
 
     @Override
