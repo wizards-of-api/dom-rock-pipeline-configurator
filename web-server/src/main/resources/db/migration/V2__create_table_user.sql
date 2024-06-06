2f3c328d90a66ce6cac8c458d1d07d9277ff84a0
AndreWakugawa <andre.wakugawa@outlook.com>
2024-06-05 23:28:03 -0300

    🗃️chore: remove is_super, add security related columns from user

--- web-server/src/main/resources/db/migration/V2__create_table_user.sql
+++ web-server/src/main/resources/db/migration/V2__create_table_user.sql
@@ -1,7 +1,11 @@
 CREATE TABLE user
 (
-    name     VARCHAR(255) NOT NULL,
-    email    VARCHAR(255) PRIMARY KEY,
-    password VARCHAR(255) NOT NULL,
-    is_super TINYINT      NOT NULL
+    name          VARCHAR(255) NOT NULL,
+    email         VARCHAR(255) PRIMARY KEY,
+    password      VARCHAR(255) NOT NULL,
+    permission_id INT,
+    created_at    TIMESTAMP,
+    updated_at    TIMESTAMP,
+
+    CONSTRAINT fk_permission_id FOREIGN KEY (permission_id) REFERENCES permission (id_permission)
 );
