49f8871a31b53b942e5044e5eaed12675f5ab183
AndreWakugawa <andre.wakugawa@outlook.com>
2024-06-05 23:37:10 -0300

    ✨feat: AuthenticationService for user authentication
    Authenticates user signup and login.
    
    Signup creates a new user with encrypted password.
    Login compares the provided credentials with what's on the database.
    
    Both return a JWT response DTO

---
+++ web-server/src/main/java/com/domrock/configurator/Services/AuthenticationService.java
@@ -0,0 +1,52 @@
+package com.domrock.configurator.Services;
+
+import com.domrock.configurator.Interface.PermissionRepository;
+import com.domrock.configurator.Interface.UserRepository;
+import com.domrock.configurator.Model.ConfigModel.DTOConfig.JwtAuhenticationResponseDTO;
+import com.domrock.configurator.Model.ConfigModel.DTOConfig.LoginRequestDTO;
+import com.domrock.configurator.Model.ConfigModel.DTOConfig.SignupRequestDTO;
+import com.domrock.configurator.Model.ConfigModel.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    private final PermissionRepository permissionRepository;
+
+    public JwtAuhenticationResponseDTO signup(SignupRequestDTO request) {
+        var user = User
+                .builder()
+                .name(request.getName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .permission(permissionRepository.findById(2).get()) // lz default
+                .build();
+
+        user = userService.saveUser(user);
+
+        var jwt = jwtService.generateToken(user);
+        return JwtAuhenticationResponseDTO.builder().token(jwt).build();
+    }
+
+    public JwtAuhenticationResponseDTO login(LoginRequestDTO request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()));
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new IllegalArgumentException("Invalid Request"));
+        var jwt = jwtService.generateToken(user);
+        return JwtAuhenticationResponseDTO.builder().token(jwt).build();
+    }
+
+}
