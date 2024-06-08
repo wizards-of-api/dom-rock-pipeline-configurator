554f8ba91b6de9fbe0fc8070a6692b97c97f238a
pauloarantesmachado <pauloantoniomachado@hotmail.com>
2024-06-08 02:24:28 -0300

    :lipstick: The variables emailLogin and passwordLogin have been added to capture the values entered in the input fields. additionally, the copyright notice ande the error message for incorrect login have been included. The loading process has also been improved.

--- web-client/src/components/Load.vue
+++ web-client/src/components/Load.vue
@@ -1,193 +1,37 @@
 <script setup lang="ts">
 </script>
-
 <template>
 	
-  <div id="load">
-    <div>G</div>
-    <div>N</div>
-    <div>I</div>
-    <div>D</div>
-    <div>A</div>
-    <div>O</div>
-    <div>L</div>
-  </div>
+	<div class="spinner loading"></div>
   
 </template>
-
 <style scoped lang="scss">
-#load {
-  position:absolute;
-  width:600px;
-  height:36px;
-  left:50%;
-  top:40%;
-  margin-left:-300px;
-  overflow:visible;
-  -webkit-user-select:none;
-  -moz-user-select:none;
-  -ms-user-select:none;
-  user-select:none;
-  cursor:default;
+.spinner.loading {
+  padding: 50px;
+  position: relative;
+  text-align: center;
 }
 
-#load div {
-  position:absolute;
-  width:20px;
-  height:36px;
-  opacity:0;
-  font-family:Helvetica, Arial, sans-serif;
-  animation:move 2s linear infinite;
-  -o-animation:move 2s linear infinite;
-  -moz-animation:move 2s linear infinite;
-  -webkit-animation:move 2s linear infinite;
-  transform:rotate(180deg);
-  -o-transform:rotate(180deg);
-  -moz-transform:rotate(180deg);
-  -webkit-transform:rotate(180deg);
-  color:var(--dr-c-blue);
+.spinner.loading:before {
+  content: "";
+  height: 100px;
+  width: 100px;
+  margin: -15px auto auto -15px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  border-width: 8px;
+  border-style: solid;
+  border-color: #222222 #00B1ED #00B1ED;
+  border-radius: 100%;
+  animation: rotation .7s infinite linear;
 }
 
-#load div:nth-child(2) {
-  animation-delay:0.2s;
-  -o-animation-delay:0.2s;
-  -moz-animation-delay:0.2s;
-  -webkit-animation-delay:0.2s;
-}
-#load div:nth-child(3) {
-  animation-delay:0.4s;
-  -o-animation-delay:0.4s;
-  -webkit-animation-delay:0.4s;
-  -webkit-animation-delay:0.4s;
-}
-#load div:nth-child(4) {
-  animation-delay:0.6s;
-  -o-animation-delay:0.6s;
-  -moz-animation-delay:0.6s;
-  -webkit-animation-delay:0.6s;
-}
-#load div:nth-child(5) {
-  animation-delay:0.8s;
-  -o-animation-delay:0.8s;
-  -moz-animation-delay:0.8s;
-  -webkit-animation-delay:0.8s;
-}
-#load div:nth-child(6) {
-  animation-delay:1s;
-  -o-animation-delay:1s;
-  -moz-animation-delay:1s;
-  -webkit-animation-delay:1s;
-}
-#load div:nth-child(7) {
-  animation-delay:1.2s;
-  -o-animation-delay:1.2s;
-  -moz-animation-delay:1.2s;
-  -webkit-animation-delay:1.2s;
-}
-
-@keyframes move {
-  0% {
-    left:0;
-    opacity:0;
+@keyframes rotation {
+  from {
+    transform: rotate(0deg);
+  } to {
+    transform: rotate(359deg);
   }
-	35% {
-		left: 41%; 
-		-moz-transform:rotate(0deg);
-		-webkit-transform:rotate(0deg);
-		-o-transform:rotate(0deg);
-		transform:rotate(0deg);
-		opacity:1;
-	}
-	65% {
-		left:59%; 
-		-moz-transform:rotate(0deg); 
-		-webkit-transform:rotate(0deg); 
-		-o-transform:rotate(0deg);
-		transform:rotate(0deg); 
-		opacity:1;
-	}
-	100% {
-		left:100%; 
-		-moz-transform:rotate(-180deg); 
-		-webkit-transform:rotate(-180deg); 
-		-o-transform:rotate(-180deg); 
-		transform:rotate(-180deg);
-		opacity:0;
-	}
-}
-
-@-moz-keyframes move {
-	0% {
-		left:0; 
-		opacity:0;
-	}
-	35% {
-		left:41%; 
-		-moz-transform:rotate(0deg); 
-		transform:rotate(0deg);
-		opacity:1;
-	}
-	65% {
-		left:59%; 
-		-moz-transform:rotate(0deg); 
-		transform:rotate(0deg);
-		opacity:1;
-	}
-	100% {
-		left:100%; 
-		-moz-transform:rotate(-180deg); 
-		transform:rotate(-180deg);
-		opacity:0;
-	}
-}
-
-@-webkit-keyframes move {
-	0% {
-		left:0; 
-		opacity:0;
-	}
-	35% {
-		left:41%; 
-		-webkit-transform:rotate(0deg); 
-		transform:rotate(0deg); 
-		opacity:1;
-	}
-	65% {
-		left:59%; 
-		-webkit-transform:rotate(0deg); 
-		transform:rotate(0deg); 
-		opacity:1;
-	}
-	100% {
-		left:100%;
-		-webkit-transform:rotate(-180deg); 
-		transform:rotate(-180deg); 
-		opacity:0;
-	}
-}
-
-@-o-keyframes move {
-	0% {
-		left:0; 
-		opacity:0;
-	}
-	35% {
-		left:41%; 
-		-o-transform:rotate(0deg); 
-		transform:rotate(0deg); 
-		opacity:1;
-	}
-	65% {
-		left:59%; 
-		-o-transform:rotate(0deg); 
-		transform:rotate(0deg); 
-		opacity:1;
-	}
-	100% {
-		left:100%; 
-		-o-transform:rotate(-180deg); 
-		transform:rotate(-180deg); 
-		opacity:0;
-	}
 }
 </style>
--- web-client/src/views/LoginView.vue
+++ web-client/src/views/LoginView.vue
@@ -1,28 +1,55 @@
 <script setup lang="ts">
 import DRButton from '@/components/DRButton.vue'
+import Load from '@/components/Load.vue';
 
+const emailLogin = defineModel<string>('emailLogin')
+const passwordLogin = defineModel<string>('passwordLogin')
+const errorLogin = defineModel<boolean>('errorLogin')
+const loadLogin = defineModel<boolean>('loadLogin')
 const login = () => {
-	console.log("Welcome")
+	loadLogin.value = true
+  setTimeout(()=>{
+    loadLogin.value = false
+  },5000)
+	console.log(emailLogin.value)
+	console.log(passwordLogin.value)
 }
 </script>
 <template>
   <main>
-  <div class="login_container">
+  <div class="load" v-if="loadLogin">
+    <Load></Load>
+  </div>
+  <div class="login_container" v-if="!loadLogin">
     <img src="../assets/dom-rock-logo.png">
+    <div class="error" v-if="errorLogin">
+      <img src="../assets/remove.png">
+      <p class="text_error">Credenciais inválidas. Verifique seu usuário e senha e tente novamente.</p>
+    </div>
     <div class="input_email">
       <h3>Usuário</h3>
-      <input type="text" class="email" placeholder="Usuário">
+      <input
+        type="text"
+        class="email"
+        placeholder="Usuário"
+        v-model="emailLogin"
+      >
     </div>
     <div class="input_password">
       <h3>Senha</h3>
-      <input type="password" class="password" placeholder="Senha">
+      <input 
+        type="password"
+        class="password"
+        placeholder="Senha"
+        v-model="passwordLogin"
+        >
     </div>
     <div class="button_login">
       <DRButton :click-behavior="login" size="large">Entrar</DRButton>
     </div>
   </div>
   </main>
-  <footer>
+  <footer v-if="!loadLogin">
     <div class="copyright">
       <p>© 2024 Wiz. Todos os direitos reservados.</p>
     </div>
@@ -68,12 +95,22 @@ input{
   gap:10px;
 }
 
-.copyright {
+.copyright{
   display: flex;
   flex-direction: column;
   align-items: center;
   margin-bottom:20px;
 }
 
+.error {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap:10px;
+}
 
+.text_error {
+  font-size: 14px;
+  color: #f2656a;
+}
 </style>
