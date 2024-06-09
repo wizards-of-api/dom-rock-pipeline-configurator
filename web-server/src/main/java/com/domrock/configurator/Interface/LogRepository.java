9f39d343ad2d85daecf1faf95e60b76abf6cc89a
AndreWakugawa <andre.wakugawa@outlook.com>
2024-06-09 16:55:12 -0300

    ✨feat: LogRepositroy with custom query by user and company name
    Custom query "findAllLogsWithUserNameAndCompanyName" is used to get the username company names by their pk email and cnpj.
    
    The list with names are used in the log table on the vue web application.

---
+++ web-server/src/main/java/com/domrock/configurator/Interface/LogRepository.java
@@ -0,0 +1,18 @@
+package com.domrock.configurator.Interface;
+
+import com.domrock.configurator.Model.ConfigModel.DTOConfig.LogViewDTO;
+import com.domrock.configurator.Model.ConfigModel.Log;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface LogRepository extends JpaRepository<Log, Integer> {
+
+    @Query(value = "SELECT new com.domrock.configurator.Model.ConfigModel.DTOConfig.LogViewDTO" +
+            "(l.id, l.logDateTime, u.name, c.fantasyName, l.action) " +
+            "FROM Log l " +
+            "JOIN User u ON l.userEmail = u.email " +
+            "JOIN Company c ON u.company.cnpj = c.cnpj", nativeQuery = true)
+    public List<LogViewDTO> findAllLogsWithUserNameAndCompanyName();
+}
