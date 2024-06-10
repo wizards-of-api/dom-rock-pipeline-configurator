0bcd5308e3cea33aae4e249c654067847dcde666
CauevSilv <caue.silva10@fatec.sp.gov.br>
2024-06-10 19:33:53 -0300

    🏗️ fix Migrations

--- web-server/src/main/resources/db/migration/V2__create_table_user.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE user
-(
-    name     VARCHAR(255) NOT NULL,
-    email    VARCHAR(255) PRIMARY KEY,
-    password VARCHAR(255) NOT NULL,
-    is_super TINYINT      NOT NULL
-);
--- web-server/src/main/resources/db/migration/V3__create_table_company.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE company
-(
-    cnpj         VARCHAR(14) PRIMARY KEY,
-    fantasy_name VARCHAR(255) NOT NULL
-);
