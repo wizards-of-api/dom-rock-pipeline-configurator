285353ea2d40d9818a3aa9fe0fb4164318735388
ocalderan <otaviocalderanadm@gmail.com>
2024-06-14 19:12:50 -0300

    ♻️ (refactor): Refactor data labels in log list

--- web-client/src/components/TableComponent.vue
+++ web-client/src/components/TableComponent.vue
@@ -1,31 +1,38 @@
 <script setup lang="ts">
-import type { Log } from './lz-config/types'
+import type { LogViewDTO } from './types'
 
 type Props = {
-	logList: Log[],
+  logList: LogViewDTO[],
 }
-const { logList} = defineProps<Props>()
+const { logList } = defineProps<Props>()
 </script>
-<template>
-	<table>
-  <caption>Tabela de log</caption>
-  <thead>
-      <th scope="col">Data</th>
-      <th scope="col">Usuário</th>
-      <th scope="col">Empresa</th>
-      <th scope="col">Ação</th>
-  </thead>
-  <tbody v-for="log in logList" :key="log.logId">
-    <tr>
-      <td data-label="Data">{{ log.logDate }}</td>
-      <td data-label="User">{{ log.logUSer }}</td>
-      <td data-label="enterprise">{{log.logInterprise}}</td>
-      <td data-label="action">{{ log.logAction}}</td>
-    </tr>
-  </tbody>
-</table>
 
+<template>
+  <table>
+    <caption>Tabela de log</caption>
+    <thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Data</th>
+        <th scope="col">Usuário</th>
+        <th scope="col">Empresa</th>
+        <th scope="col">Ação</th>
+        <th scope="col">Código de Resposta</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="log in logList" :key="log.id">
+        <td data-label="ID">{{ log.id }}</td>
+        <td data-label="Data">{{ log.logDateTime }}</td>
+        <td data-label="Usuário">{{ log.userName }}</td>
+        <td data-label="Empresa">{{ log.companyName }}</td>
+        <td data-label="Ação">{{ log.action }}</td>
+        <td data-label="Código de Resposta">{{ log.responseCode }}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
+
 <style scoped lang="scss">
 table {
   border: 1px solid #ccc;
@@ -42,7 +49,7 @@ table caption {
   margin: 0.5em 0 0.75em;
   text-align: left;
   font-weight: bold;
-  color: #333;
+  color: #ddd;
 }
 
 table thead {
@@ -123,8 +130,6 @@ table td {
     border-bottom: 0;
   }
 }
-
-/* General styling */
 body {
   font-family: "Open Sans", sans-serif;
   line-height: 1.5;
