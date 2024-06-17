8340138239aca2bb56ef82f4aec286e9c2632b5f
ChavesVini <vinidasilva2004@outlook.com>
2024-06-16 23:13:18 -0300

    :alien: changed the query

--- web-server/src/main/java/com/domrock/configurator/Interface/CompanyRepository.java
+++ web-server/src/main/java/com/domrock/configurator/Interface/CompanyRepository.java
@@ -8,9 +8,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface CompanyRepository extends JpaRepository<Company, String> {
-    @Query(value = "SELECT c.fantasy_name, COUNT(uc.user_email) FROM company c " +
-                   "JOIN user_company uc ON c.cnpj = uc.company_cnpj " +
-                   "GROUP BY c.fantasy_name", nativeQuery = true)
+    @Query(value = "SELECT c.fantasy_name, COUNT(u.email) FROM company c " +
+                    "JOIN user u ON c.cnpj = u.company_cnpj" + 
+                    "GROUP BY c.fantasy_name", nativeQuery = true)
+
     List<Object[]> getNumberOfUsersByCompany();
 
     @Query(value = "select c.fantasy_name, count(lz.file_id) as contagem from company c join lz_config lz on lz.cnpj = c.cnpj group by c.fantasy_name", nativeQuery = true)
